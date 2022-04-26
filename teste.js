1db8cf71a278837b9e0afe03f5e80b26996d7ffc
Asael Honório <asaelhms@gmail.com>
2022-04-26 10:26:27 -0300

    .

--- teste.js
+++ teste.js
@@ -1,6 +1,6 @@
 var num1 = document.getElementById('num1')
 var num2 = document.getElementById('num2')
-var divNum = document.querySelectorAll('.numeros')
+var divNum = document.querySelector('.numeros')
 var res = document.createElement('div')
 res.setAttribute('id','res')
 
@@ -9,6 +9,12 @@ function somar(){
     var n2 = Number(num2.value)
     var s = n1+n2
 
+        if(n1 || n2 < 0 || ''){
+            res.innerHTML = 'Adicione um número válido'
+        }
+        else{
+            res.innerHTML = `A soma dos números ${n1} + ${n2} é igual a = ${s}`
+        }
 }
-divNum.append
+divNum.appendChild(res)
 console.log(divNum)
