4d78906a30719c5ab703101814806c66fc5cb3f7
Asael Honório <asaelhms@gmail.com>
2022-04-26 10:10:16 -0300

    praticando

---
+++ teste.js
@@ -0,0 +1,14 @@
+var num1 = document.getElementById('num1')
+var num2 = document.getElementById('num2')
+var divNum = document.querySelectorAll('.numeros')
+var res = document.createElement('div')
+res.setAttribute('id','res')
+
+function somar(){
+    var n1 = Number(num1.value)
+    var n2 = Number(num2.value)
+    var s = n1+n2
+
+}
+divNum.append
+console.log(divNum)
